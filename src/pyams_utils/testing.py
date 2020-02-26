b6c4692861306709f4b05479875597405a553d8c
Thierry Florac <thierry.florac@onf.fr>
2020-02-26 18:34:13 +0100

    Added "fanstatic" module testing resources

--- src/pyams_utils/testing.py
+++ src/pyams_utils/testing.py
@@ -16,8 +16,14 @@
 This module provides small testing helpers.
 """
 
+import sys
+
 import lxml.etree
 import lxml.html
+
+from fanstatic import Library, Resource
+from fanstatic.core import set_resource_file_existence_checking
+
 
 __docformat__ = 'restructuredtext'
 
@@ -43,31 +49,31 @@ def render_xpath(view, xpath='.'):
         return string
 
     try:
-        root = lxml.etree.fromstring(string)
-    except lxml.etree.XMLSyntaxError:
+        root = lxml.etree.fromstring(string)  # pylint: disable=c-extension-no-member
+    except lxml.etree.XMLSyntaxError:  # pylint: disable=c-extension-no-member
         root = lxml.html.fromstring(string)
 
-    output = ""
-    for node in root.xpath(
-        xpath, namespaces={'xmlns': 'http://www.w3.org/1999/xhtml'}):
-        s = lxml.etree.tounicode(node, pretty_print=True)
-        s = s.replace(' xmlns="http://www.w3.org/1999/xhtml"', ' ')
-        output += s
+    result = ""
+    for node in root.xpath(xpath, namespaces={'xmlns': 'http://www.w3.org/1999/xhtml'}):
+        # pylint: disable=c-extension-no-member
+        out = lxml.etree.tounicode(node, pretty_print=True)
+        out = out.replace(' xmlns="http://www.w3.org/1999/xhtml"', ' ')
+        result += out
 
-    if not output:
+    if not result:
         raise ValueError("No elements matched by %s." % repr(xpath))
 
     # let's get rid of blank lines
-    output = output.replace('\n\n', '\n')
+    result = result.replace('\n\n', '\n')
 
     # self-closing tags are more readable with a space before the
     # end-of-tag marker
-    output = output.replace('"/>', '" />')
+    result = result.replace('"/>', '" />')
 
-    return output
+    return result
 
 
-def format_html(input):
+def format_html(input):  # pylint: disable=redefined-builtin
     """Render formatted HTML code by removing empty lines and spaces ending lines
 
     >>> from pyams_utils.testing import format_html
@@ -75,3 +81,13 @@ def format_html(input):
     '<div>\\n<b>This is a test</b>\\n</div>'
     """
     return '\n'.join(filter(bool, map(str.rstrip, input.split('\n'))))
+
+
+if sys.argv[-1].endswith('/bin/test'):
+
+    library = Library('foo', '')  # pylint: disable=invalid-name
+
+    set_resource_file_existence_checking(False)
+    res_x1 = Resource(library, 'x1.js')  # pylint: disable=invalid-name
+    res_x1.dependency_nr = 0
+    set_resource_file_existence_checking(True)
