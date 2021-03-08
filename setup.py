f1686985b25e2301e00ea8c071ff6726039e86bb
Thierry Florac <thierry.florac@onf.fr>
2021-03-08 09:00:55 +0100

    Version 1.7.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ DOCS = os.path.join(os.path.dirname(__file__),
 README = os.path.join(DOCS, 'README.txt')
 HISTORY = os.path.join(DOCS, 'HISTORY.txt')
 
-version = '1.6.1'
+version = '1.7.0'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 tests_require = [
@@ -70,6 +70,9 @@ setup(name='pyams_utils',
           'babel',
           'beaker',
           'chameleon',
+          'colander',
+          'cornice',
+          'cornice_swagger',
           'docutils',
           'fanstatic',
           'httplib2',
