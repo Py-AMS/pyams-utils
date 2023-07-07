d815d1b865bd867700eebee8259679eaa676860a
Thierry Florac <tflorac@ulthar.net>
2023-07-07 13:38:56 +0200

    Moved proxy information widget factory to PyAMS_zmi package (to avoid circular references with PyAMS_form package)

--- src/pyams_utils/protocol/http.py
+++ src/pyams_utils/protocol/http.py
@@ -20,15 +20,9 @@ import urllib.parse
 import httplib2
 
 from persistent import Persistent
-from zope.interface import Interface
 from zope.schema.fieldproperty import FieldProperty
 
-from pyams_form.interfaces import IObjectFactory
-from pyams_form.interfaces.form import IForm
-from pyams_form.interfaces.widget import IObjectWidget
-from pyams_layer.interfaces import IFormLayer
-from pyams_utils.adapter import adapter_config
-from pyams_utils.factory import factory_config, get_interface_name, get_object_factory
+from pyams_utils.factory import factory_config
 from pyams_utils.interfaces.proxy import IProxyInfo
 
 
@@ -59,14 +53,6 @@ class ProxyInfo(Persistent):
                                       if self.username else '',
                                       self.host,
                                       self.port)
-
-
-@adapter_config(name=get_interface_name(IProxyInfo),
-                required=(Interface, IFormLayer, IForm, IObjectWidget),
-                provides=IObjectFactory)
-def proxy_info_factory(*args, **kwargs):
-    """Proxy information object widget factory"""
-    return get_object_factory(IProxyInfo)
 
 
 class HTTPClient:
